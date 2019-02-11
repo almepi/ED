52ee970972bf713241b79940438a46d148586d43
almepi <alejandro.megias22@gmail.com>
2019-02-11 09:57:09 +0100

    SumaAndroid

---
+++ AndroidProject/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage (View view){
+
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+        EditText editText = findViewById(R.id.editText) ;
+
+        System.out.println("Ejecutando SendMessage editText.getText().toString()="+ editText.getText().toString());
+    }
+}
---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        buttonaceptar.Clicked += delegate {
+        
+
+        };
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButtonaceptarClicked(object sender, EventArgs e){
+        string nombre = entry1.Text;
+        labelsaludo.Text = "Hola " + nombre;
+        Console.WriteLine("Has Hecho click en Aceptar");
+    }
+}
